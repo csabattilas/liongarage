585376f00f485195f2f3431fb451105b69eeaedf
csaba szabo <csabattilaszabo@gmail.com>
2021-05-05 22:18:23 +0200

    add client side types based on graphql types

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Warehouse = {
+  __typename?: 'Warehouse';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  cars: Cars;
+};
+
+export type Cars = {
+  __typename?: 'Cars';
+  location?: Maybe<Scalars['String']>;
+  vehicles: Array<Maybe<Vehicle>>;
+};
+
+export type Vehicle = {
+  __typename?: 'Vehicle';
+  id: Scalars['ID'];
+  make: Scalars['String'];
+  model: Scalars['String'];
+  year_model: Scalars['Int'];
+  price: Scalars['Float'];
+  licensed: Scalars['Boolean'];
+  date_added: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  allCars: Array<Vehicle>;
+  warehouses: Array<Warehouse>;
+};
